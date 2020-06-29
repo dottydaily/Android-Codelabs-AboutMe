1c3a0dbfe4529b46a7e145fe3c8db588e708d9a6
Pornpat Santibuppakul <dottypurkt@gmail.com>
2020-06-29 15:47:08 +0700

    Add Section 02.2: Add user interactivity

--- app/src/main/java/org/aboutme/codelab/MainActivity.kt
+++ app/src/main/java/org/aboutme/codelab/MainActivity.kt
@@ -1,7 +1,12 @@
 package org.aboutme.codelab
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,5 +15,40 @@ class MainActivity : AppCompatActivity() {
 
         // connect the layout file with the Activity
         setContentView(R.layout.activity_main)
+
+        done_button.setOnClickListener {
+            addNickname(it)
+        }
+
+        nickname_text.setOnClickListener {
+            updateNickname(it)
+        }
+    }
+
+    // Helper method
+    private fun addNickname(view: View) {
+        if (nickname_text.text.isBlank()) {
+            Toast.makeText(this, "Enter your nickname first!", Toast.LENGTH_SHORT).show()
+        } else {
+            nickname_text.apply {
+                text = nickname_edit.text
+                visibility = View.VISIBLE
+            }.also {
+                nickname_edit.visibility = View.GONE
+                done_button.visibility = View.GONE
+            }
+
+            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+        }
+    }
+
+    private fun updateNickname(view: View) {
+        let {
+            nickname_edit.visibility = View.VISIBLE
+            done_button.visibility = View.VISIBLE
+        }.also {
+            view.visibility = View.GONE
+        }
     }
 }
